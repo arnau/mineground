aaf82c2bae13c8c8c270e8d3e15c4e3b3b5800c4
Arnau Siches <asiches@gmail.com>
2017-08-10 09:34:07 +0100

    Initial state
    Signed-off-by: Arnau Siches <asiches@gmail.com>

---
+++ lib/mineclient.ex
@@ -0,0 +1,18 @@
+defmodule Mineclient do
+  @moduledoc """
+  Documentation for Mineclient.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mineclient.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
